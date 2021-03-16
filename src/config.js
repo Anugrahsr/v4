7dcaa266d063bf495a53f1b6aaafef9a2984e378
anugrahsr <asrmpr@gmail.com>
2021-03-16 10:04:17 +0530

    update

--- src/config.js
+++ src/config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/anugrahsr',
+      url: 'https://www.linkedin.com/in/anugrah-sr',
     },
   ],
 
